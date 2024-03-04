f887ae38aee501957bda98997ea0cdedb51d69a8
Shruti Teltumbade <93871770+10Shruti@users.noreply.github.com>
2024-03-04 10:37:13 +0530

    Update SignUpController.java

--- main/java/com/practice/Controller/SignUpController.java
+++ main/java/com/practice/Controller/SignUpController.java
@@ -2,16 +2,24 @@ package com.practice.Controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.practice.Entity.User;
 import com.practice.repository.UserRepository;
+
 
 
 @Controller
@@ -19,8 +27,9 @@ public class SignUpController {
 
 	@Autowired
 	UserRepository userRepo;
+	
 
-	@RequestMapping("/hi")
+	@RequestMapping("/index")
 	public String indexPage() {
 		return "index";
 	}
@@ -28,32 +37,40 @@ public class SignUpController {
 	@GetMapping("/Register")
 	public String registerPage() {
 		return "Register";
-	}
-		
-		
-	@PostMapping("/Home")
-	public  String homeForm(@ModelAttribute User user ) {
-		userRepo.save(user);
-		return "Home";
-	}
-			
-	@GetMapping("/get")
-	public List<User> getformvalu1e() {
-		System.out.println(userRepo.findAll());
-		 return userRepo.findAll();
-	}
+	}		 	
 	
-	
-	@RequestMapping("/user/{email}")
-	public String deleteEmployee(@PathVariable String email) {
-		userRepo.deleteById(email);
-		return "Home";
-	}
-	
-	
-	
-}
+	@PostMapping("/Home") 
+    @ResponseBody
+    public String homeForm(@ModelAttribute User user) {
+        try {
+            userRepo.save(user);
+            return "Home";
+            
+        } catch (DataIntegrityViolationException e) {
+            return "Duplicate email address";
+        }
+        
+    }
 
+	
+	@GetMapping("/get")
+    public ResponseEntity<List<User>> getFormValue() {
+        List<User> users = userRepo.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+	
+    @RequestMapping("/delete/{email}")
+    public ResponseEntity<String> deleteUserByEmail(@PathVariable String email) {
+    	User user = userRepo.findByEmail(email);
+        if (user != null) {
+            userRepo.delete(user);
+            return ResponseEntity.ok("User with email " + email + " deleted successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User with email " + email + " not found");
+            
+        }
+    }
+}
 
 
 
